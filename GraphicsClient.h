22f3f4bac1679f378f4bb0145093422d9e534de2
Haadi M <haadi.majeed99@gmail.com>
2021-11-19 03:05:14 -0600

    too much space

--- GraphicsClient.h
+++ GraphicsClient.h
@@ -5,39 +5,22 @@ class GraphicsClient
 {
 public:
     GraphicsClient(string _addr, int _port);
-
     GraphicsClient(GraphicsClient &copy);
-
     ~GraphicsClient();
-
     void operator=(string newAddr);
-
     int connection();
-
     int closeConnection();
-
     void setBackgroundcolor(int r, int g, int b);
-
     void setDrawingcolor(int r, int g, int b);
-
     void clear();
-
     void setPixel(int x, int y, int r, int g, int b);
-
     void drawRectangle(int x, int y, int w, int h);
-
     void fillRectangle(int x, int y, int w, int h);
-
     void clearRectangle(int x, int y, int w, int h);
-
     void drawOval(int x, int y, int w, int h);
-
     void fillOval(int x, int y, int w, int h);
-
     void drawline(int x, int y, int w, int h);
-
     void drawString(int x, int y, string out);
-
     void repaint();
 
 private:
